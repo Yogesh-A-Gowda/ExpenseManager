d7cf8d53bab97d081374c61a7fc5f6120eb4068f
Yogesh-A-Gowda <67061755+Yogesh-A-Gowda@users.noreply.github.com>
2024-12-26 23:27:58 +0530

    Update database.js

--- Server/libs/database.js
+++ Server/libs/database.js
@@ -7,8 +7,5 @@ const { Pool } = pg;
 
 export const pool = new Pool({
   connectionString: process.env.DB_URL,
-  ssl: {
-    rejectUnauthorized: false, // Allows self-signed certificates
-    ca: fs.readFileSync("C:/Users/yogesh/Downloads/ca.pem").toString(), // Path to your CA certificate
-  },
+  ssl: { rejectUnauthorized: false, },
 });
